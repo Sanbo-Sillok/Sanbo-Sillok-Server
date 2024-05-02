de3f4dedaa149c6097a0b5f8e0809373a7ef6a26
yang <hc1489@naver.com>
2024-05-02 16:57:44 +0900

    [setting] 프로젝트 환경 설정

---
+++ src/main/java/com/sanbosillok/sanbosillokserver/SanbosillokserverApplication.java
@@ -0,0 +1,13 @@
+package com.sanbosillok.sanbosillokserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanbosillokserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanbosillokserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanbosillok/sanbosillokserver/SanbosillokserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanbosillok.sanbosillokserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanbosillokserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
