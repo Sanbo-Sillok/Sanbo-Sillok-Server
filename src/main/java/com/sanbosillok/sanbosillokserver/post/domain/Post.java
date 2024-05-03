6a010e0432f320314cb03d6af4e6f01011d775f9
yang <hc1489@naver.com>
2024-05-03 18:12:22 +0900

    feat: Post Entity 구현

---
+++ src/main/java/com/sanbosillok/sanbosillokserver/post/domain/Post.java
@@ -0,0 +1,43 @@
+package com.sanbosillok.sanbosillokserver.post.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Post {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String contents;
+
+    private String lastModifier;
+
+    private PostStatus status;
+
+    @Builder
+    public Post(String title, String contents, String lastModifier, PostStatus status) {
+        this.title = title;
+        this.contents = contents;
+        this.lastModifier = lastModifier;
+        this.status = status;
+    }
+
+    public void update(String contents, String lastModifier) {
+        this.contents = contents;
+        this.lastModifier = lastModifier;
+    }
+
+}
