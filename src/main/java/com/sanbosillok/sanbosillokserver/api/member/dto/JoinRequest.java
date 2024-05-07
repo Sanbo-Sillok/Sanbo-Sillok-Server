2530e7b2505bffa0298295cf8888d580cae6c54b
yang <hc1489@naver.com>
2024-05-07 14:03:16 +0900

    feat: 회원가입을 위한 DTO 구현

---
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/member/dto/JoinRequest.java
@@ -0,0 +1,11 @@
+package com.sanbosillok.sanbosillokserver.api.member.dto;
+
+import lombok.Data;
+
+@Data
+public class JoinRequest {
+
+    private String username;
+
+    private String password;
+}
