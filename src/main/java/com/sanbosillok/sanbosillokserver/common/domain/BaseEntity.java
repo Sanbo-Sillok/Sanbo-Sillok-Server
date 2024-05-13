681dbd81a6ce2309713af915c8841681987ddd69
yang <hc1489@naver.com>
2024-05-14 00:04:56 +0900

    refactor: BaseEntity 칼럼 명 수정

--- src/main/java/com/sanbosillok/sanbosillokserver/common/domain/BaseEntity.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/common/domain/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.sanbosillok.sanbosillokserver.common.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,9 +15,11 @@ import java.time.LocalDateTime;
 @Getter
 public class BaseEntity {
 
+    @Column(name = "created_at")
     @CreatedDate
     private LocalDateTime createdAt;
 
+    @Column(name = "updated_at")
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
