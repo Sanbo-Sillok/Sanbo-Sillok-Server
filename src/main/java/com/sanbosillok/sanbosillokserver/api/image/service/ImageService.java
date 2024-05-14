2718cf19686ce687e2a3affed0e60fe9fe56a543
yang <hc1489@naver.com>
2024-05-14 17:15:46 +0900

    feat: Image DTO 생성 및 Service 반환 형태 DTO로 변경

--- src/main/java/com/sanbosillok/sanbosillokserver/api/image/service/ImageService.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/image/service/ImageService.java
@@ -1,14 +1,19 @@
 package com.sanbosillok.sanbosillokserver.api.image.service;
 
+import com.sanbosillok.sanbosillokserver.api.image.dto.ImagePathResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class ImageService {
 
-    public void upload(String path, MultipartFile file) {
+    public ImagePathResponse upload(String path, MultipartFile file) {
         String uuid = UUID.randomUUID().toString();
         String fileName = file.getOriginalFilename();
         assert fileName != null;
@@ -18,6 +23,7 @@ public class ImageService {
 
         try {
             file.transferTo(saveFile);
+            return new ImagePathResponse(path + uuid + fileExtension);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
