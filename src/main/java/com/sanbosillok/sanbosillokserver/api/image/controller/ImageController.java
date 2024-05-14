f3f7b98bc9cf7185432e1673b62a176bb1b3dc2e
yang <hc1489@naver.com>
2024-05-15 01:18:53 +0900

    fix: image 반환 contetn-type 변경

--- src/main/java/com/sanbosillok/sanbosillokserver/api/image/controller/ImageController.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/image/controller/ImageController.java
@@ -2,15 +2,13 @@ package com.sanbosillok.sanbosillokserver.api.image.controller;
 
 import com.sanbosillok.sanbosillokserver.api.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +16,7 @@ public class ImageController {
 
     private final ImageService imageService;
 
-    @GetMapping("/image/{fileName}")
+    @GetMapping(value = "/image/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] getImage(@PathVariable String fileName){
         return imageService.getImage(fileName);
     }
