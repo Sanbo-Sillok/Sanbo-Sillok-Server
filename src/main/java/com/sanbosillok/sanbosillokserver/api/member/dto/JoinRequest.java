122c6090df916459caaaf79e5ac7b4f2f48dfec8
yang <hc1489@naver.com>
2024-05-14 17:25:51 +0900

    feat: 회원가입 request type을 multipart로 변경

--- src/main/java/com/sanbosillok/sanbosillokserver/api/member/dto/JoinRequest.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/member/dto/JoinRequest.java
@@ -1,6 +1,7 @@
 package com.sanbosillok.sanbosillokserver.api.member.dto;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class JoinRequest {
@@ -8,4 +9,6 @@ public class JoinRequest {
     private String username;
 
     private String password;
+
+    private MultipartFile studentIdImage;
 }
