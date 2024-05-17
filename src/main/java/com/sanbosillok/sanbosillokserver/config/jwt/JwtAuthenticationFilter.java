62ea9362671e554ae2b33f09783175bf41ee0568
yang <hc1489@naver.com>
2024-05-17 19:54:45 +0900

    feat: 인가 담당 Filter 구현

---
+++ src/main/java/com/sanbosillok/sanbosillokserver/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.sanbosillok.sanbosillokserver.config.jwt;
+
+import com.sanbosillok.sanbosillokserver.api.auth.dto.CustomUserDetails;
+import com.sanbosillok.sanbosillokserver.api.member.domain.Member;
+import com.sanbosillok.sanbosillokserver.api.member.domain.MemberRole;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization= request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            throw new IllegalArgumentException("잘못된 토큰 형식입니다.");
+        }
+
+        String token = authorization.split(" ")[1];
+
+        if (jwtTokenProvider.isExpired(token)) {
+            filterChain.doFilter(request, response);
+            throw new IllegalArgumentException("토큰이 만료되었습니다.");
+        }
+
+        String username = jwtTokenProvider.getUsername(token);
+        MemberRole role = MemberRole.valueOf(jwtTokenProvider.getRole(token));
+
+        //userEntity를 생성하여 값 set
+        Member member = Member.builder()
+                .username(username)
+                .password("tempPassword")
+                .role(role)
+                .build();
+
+        //UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(member);
+
+        //스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        //세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
