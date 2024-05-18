d413f82feb6b26cc12b46f940ef15ba52e00ee8a
yang <hc1489@naver.com>
2024-05-18 15:03:14 +0900

    refactor: 게시글 작성자 이름 마스킹

--- src/main/java/com/sanbosillok/sanbosillokserver/api/post/dto/PostResponse.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/post/dto/PostResponse.java
@@ -11,6 +11,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PostResponse {
 
+    private static final String MASK_CHAR = "*****";
+
     private Long id;
     private String title;
     private String content;
@@ -23,7 +25,7 @@ public class PostResponse {
         id = post.getId();
         title = post.getTitle();
         content = post.getContent();
-        lastModifier = post.getLastModifier();
+        lastModifier = post.getLastModifier().substring(0, 3) + MASK_CHAR;
         status = post.getStatus();
         updatedAt = post.getUpdatedAt();
         isExist = (id != null);
