f9b712374dd6b12fd4dc5e4e0f14df5db50f69ab
yang <hc1489@naver.com>
2024-05-18 23:27:24 +0900

    refactor: 로그인 시, Request 및 Response Parsing 방식 변경

--- src/main/java/com/sanbosillok/sanbosillokserver/config/jwt/LoginFilter.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/config/jwt/LoginFilter.java
@@ -1,12 +1,13 @@
 package com.sanbosillok.sanbosillokserver.config.jwt;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sanbosillok.sanbosillokserver.api.auth.dto.CustomUserDetails;
+import com.sanbosillok.sanbosillokserver.api.auth.dto.TokenResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,6 +15,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -21,20 +23,27 @@ import java.util.Iterator;
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String username = obtainUsername(request);
-        String password = obtainPassword(request);
+        JsonNode jsonNode;
+        try {
+            jsonNode = objectMapper.readTree(request.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        String username = jsonNode.get("username").asText();
+        String password = jsonNode.get("password").asText();
+
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
 
         return authenticationManager.authenticate(authToken);
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
 
         String username = customUserDetails.getUsername();
@@ -47,7 +56,8 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String token = jwtTokenProvider.createJwt(username, role);
 
-        response.addHeader("Authorization", "Bearer " + token);
+        response.setContentType("application/json");
+        response.getWriter().print(objectMapper.writeValueAsString(new TokenResponse(token)));
     }
 
     @Override
