5d9a80c872b622201617f366ac8eb608418cc865
yang <hc1489@naver.com>
2024-05-24 17:31:49 +0900

    fix: 회원 가입 승인 전 user list 반환 시, id 를 반환하지 않는 문제 해결

--- src/main/java/com/sanbosillok/sanbosillokserver/api/member/dto/MemberResponse.java
+++ src/main/java/com/sanbosillok/sanbosillokserver/api/member/dto/MemberResponse.java
@@ -7,11 +7,13 @@ import java.time.LocalDateTime;
 
 @Data
 public class MemberResponse {
+    private Long id;
     private String username;
     private LocalDateTime createdAt;
     private String studentIdImagePath;
 
     public MemberResponse(Member member) {
+        id = member.getId();
         username = member.getUsername();
         createdAt = member.getCreatedAt();
         studentIdImagePath = member.getStudentIdImagePath();
